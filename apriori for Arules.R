b924ac8d2ef5a345dcf34caaeda535cd4733af1f
Sumeet Santani <ssantani@syr.edu>
2018-08-17 13:16:46 -0400

    Update apriori for Arules.R

--- apriori for Arules.R
+++ apriori for Arules.R
@@ -1,15 +1,17 @@
-# Apriori
+# Association rules using Apriori
 
-# Data Preprocessing
-# install.packages('arules')
+# Step 1: Let's process the data
+install.packages('arules')
 library(arules)
+
+#Importing the dataset in R
 dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
-dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE) #Removing duplicate enteries
 summary(dataset)
 itemFrequencyPlot(dataset, topN = 10)
 
-# Training Apriori on the dataset
+# Step 2: Let's train Apriori on the dataset
 rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
 
-# Visualising the results
+# Step 3: Visualising the results
 inspect(sort(rules, by = 'lift')[1:10])
